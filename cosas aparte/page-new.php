2920c2097f27a8cd83e10f1515fff8ab5af50a08
riverlc621 <149281720+riverlc621@users.noreply.github.com>
2023-10-28 21:41:02 -0500

    Add files via upload

---
+++ cosas aparte/page-new.php
@@ -0,0 +1,21 @@
+<!-- <!DOCTYPE html>
+<?php
+
+session_start();
+session_destroy();
+
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+<div>cerrado por inactividad</div>
+
+
+</body>
+</html> -->
